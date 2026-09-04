19c69d6bd19697305622555e4ebcbb3b7e2ae0a1
Dudu <ratombodm@gmail.com>
2026-09-05 00:40:37 +0200

    chore: initialise VERA layered solution

---
+++ VERA.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VERA.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
