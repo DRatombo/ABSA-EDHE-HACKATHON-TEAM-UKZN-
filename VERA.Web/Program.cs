d5e00885c6319662c5740f68fae28618b362eb7a
Bayanda-J <banelejwara2@gmail.com>
2026-09-05 05:39:25 +0200

    UI For Home page

--- VERA.Web/Program.cs
+++ VERA.Web/Program.cs
@@ -1,19 +1,28 @@
 using Microsoft.EntityFrameworkCore;
-using VERA.Business.Services;
 using VERA.Data.Context;
+using VERA.Business.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add MVC support.
-builder.Services.AddControllersWithViews();
-
-// Connect Entity Framework Core to Microsoft SQL Server.
-builder.Services.AddDbContext<VeraDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // ---------------------------------------------------------
-// REGISTER VERA BUSINESS SERVICES
+// MVC
+// ---------------------------------------------------------
+
+builder.Services.AddControllersWithViews();
+
+
+// ---------------------------------------------------------
+// DATABASE
+// ---------------------------------------------------------
+
+builder.Services.AddDbContext<VeraDbContext>(options =>
+	options.UseSqlServer(
+		builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// ---------------------------------------------------------
+// VERA BUSINESS SERVICES
 // ---------------------------------------------------------
 
 builder.Services.AddScoped<FundingCalculatorService>();
@@ -25,6 +34,44 @@ builder.Services.AddScoped<OpportunityDecisionService>();
 builder.Services.AddScoped<FulfilmentPassportService>();
 builder.Services.AddScoped<OpportunityAssessmentService>();
 
+
+// ---------------------------------------------------------
+// BUILD APPLICATION
+// ---------------------------------------------------------
+
 var app = builder.Build();
 
-// Keep the rest of your existing middleware below.
+
+// ---------------------------------------------------------
+// HTTP REQUEST PIPELINE
+// ---------------------------------------------------------
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+// ---------------------------------------------------------
+// MVC ROUTING
+// ---------------------------------------------------------
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+// ---------------------------------------------------------
+// START APPLICATION
+// ---------------------------------------------------------
+
+app.Run();
