f4fb6ea925dde94a5cab98e4ad3c24089636593b
AkhonaSM35 <aksphumelele345@gmail.com>
2026-09-05 14:52:01 +0200

    feat: add register choice,

--- VERA.Web/Controllers/AccountController.cs
+++ VERA.Web/Controllers/AccountController.cs
@@ -4,35 +4,35 @@ using VERA.Models.ViewModels;
 
 namespace VERA.Web.Controllers
 {
-	public class AccountController : Controller
-	{
-		[HttpGet]
-		public IActionResult Register()
-		{
-			return View();
-		}
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
 
-		// your Register POST remains here
+        // your Register POST remains here
 
 
-		[HttpGet]
-		public IActionResult Login()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Login(LoginViewModel model)
-		{
-			if (!ModelState.IsValid)
-			{
-				return View(model);
-			}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
 
-			TempData["LoginSuccess"] = "Welcome back to VERA.";
+            TempData["LoginSuccess"] = "Welcome back to VERA.";
 
-			return RedirectToAction("Index", "Home");
-		}
-	}
+            return RedirectToAction("Index", "Home");
+        }
+    }
 }
